b003f7f5b1d7cfd8d9b61fe2b3c26ab9c746d1b3
Jesuislacarotte <drouinbastien@gmail.com>
2022-04-25 15:26:56 -0400

    Script and folders
    new scripts + folders for scenes

---
+++ Project Card Trader/Assets/2 script/Card script/Card_Stat.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Card_Stat : MonoBehaviour
+{
+    [Header("Valeurs modifiables")]
+    [SerializeField] private float Var1;
+    [SerializeField] private float Var2;
+    [SerializeField] private float Var3;
+    [SerializeField] private float Var4;
+
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
